1d14c69f595d7c17f5edaed9b1e431752b222801
Diego Sosa <diego.venezuela@gmail.com>
2017-11-30 23:13:27 +0100

    Minimal Dice + test working

--- models/dice.go
+++ models/dice.go
@@ -1,23 +1,30 @@
 package models
 
-import("math/rand")
+import (
+    "math/rand"
+    "time"
+)
 
-const DICE_SIDES=6
+const DICE_SIDES = 6
 
-type Dice Struct{
-    value  int
+type Dice struct {
+	value int
 }
 
-func NewDice()Dice{
-   return Dice{}
+func NewDice() Dice {
+	return Dice{}
 }
 
-func( d *Dice ) Shake (int){
-    rand.Seed(42)
+func (d Dice) Shake()int {
+
+    now := time.Now()
+    nanoSeed := now.UnixNano()
+
+    rand.Seed(nanoSeed)
     d.value = rand.Intn(DICE_SIDES)
-    return *d.value
+    return d.value
 }
 
-func( d *Dice ) GetValue(int){
-    return *d.Value
+func (d Dice) GetValue()int {
+	return d.value
 }
--- models/dice_test.go
+++ models/dice_test.go
@@ -1,7 +1,13 @@
 package models
 
+import(
+    "github.com/davecgh/go-spew/spew"
+    "testing"
+)
+
+
 func TestDice(t *testing.T) {
-   dice := models.NewDice()
-   value := dice.Shake()
-   spew.Dump(value)
+	dice := NewDice()
+	value := dice.Shake()
+	spew.Dump(value)
 }
