12ae3b45224d35b34d21c14dc0c3d7024689b8c3
Diego Sosa <diego.venezuela@gmail.com>
2017-12-02 10:08:43 +0100

    Basic implementation of TCP listen

--- server/server.go
+++ server/server.go
@@ -3,6 +3,10 @@ package server
 import(
 	"github.com/diegoseso/parchis/models"
 	"github.com/davecgh/go-spew/spew"
+	"net"
+	"log"
+	"io"
+	"time"
 )
 
 type Server struct{
@@ -14,19 +18,45 @@ func NewServer() *Server{
 }
 
 func(S *Server)Run(){
+    S.listen()
+	S.initializeRoomsFromConfig()
+	S.iniitializeBoardsOnRoom()
+}
 
-	InitializeRoomsFromConfig()
-	IniitializeBoardsOnRoom()
+func(S *Server)listen(){
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal()
+	}
+	for{
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		handleConn(conn)
+	}
+}
+
+func handleConn( c net.Conn){
+	defer c.Close()
+	for {
+		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
+		if err != nil {
+			return
+		}
+		time.Sleep(1 * time.Millisecond)
+	}
 }
 
 func(S *Server)Stop(){
 }
 
-func IniitializeBoardsOnRoom(){
+func(S *Server)iniitializeBoardsOnRoom(){
 
 }
 
-func InitializeRoomsFromConfig(){
+func(S *Server)initializeRoomsFromConfig(){
 	room := &models.Room{}
 	AddBoardToRoom(room)
 }
