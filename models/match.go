3c80144eafd9d635cfa37909c97d322257af8611
diegoseso <diegoseso@developer>
2018-02-12 23:49:09 +0100

    Playable Match

--- models/match.go
+++ models/match.go
@@ -11,84 +11,89 @@ type Match struct {
 	//History *History
 }
 
+func NewMatch() *Match {
+    id, _ := uuid.NewV4()
+    idString := id.String()
+
+    return &Match{
+        Id:          idString,
+        MatchStatus: NewMatchStatus(),
+    }
+}
+
+type TurnControl int
+
 type MatchStatus struct {
 	BoardPicture map[int]PlayerStatus
 }
 
-type PlayerStatus map[int]byte
-
 func NewMatchStatus() *MatchStatus {
-	// Here we are gonna make a bytes map
-	bs := make(map[int]PlayerStatus)
+    // Here we are gonna make a bytes map
+    bs := make(map[int]PlayerStatus)
 
-	for i := 1; i <= 4; i++ {
-		bs[i] = PlayerStatus{1: 0, 2: 0, 3: 0, 4: 0}
-	}
+    for i := 1; i <= 4; i++ {
+        bs[i] = PlayerStatus{1: 0, 2: 0, 3: 0, 4: 0}
+    }
 
-	return &MatchStatus{BoardPicture: bs}
+    return &MatchStatus{BoardPicture: bs}
 }
 
 func (ms *MatchStatus) CalculatePosition(player int, toAddValue int) int {
 
-	for k, v := range ms.BoardPicture[player] {
-		if int(v)+toAddValue > 75 {
-			// This is the user bouncing
-			ms.BoardPicture[player][k] = byte(75 - (int(v) + toAddValue - 75))
+    // @TODO quite a challenge get the algorithm for the best choice
 
-			return int(ms.BoardPicture[player][k])
-		}
-		ms.BoardPicture[player][k] += byte(toAddValue)
-		return int(ms.BoardPicture[player][k])
-	}
-	// @TODO This is gonna be a bug one day
-	return 0
+    for k, v := range ms.BoardPicture[player] {
+        if int(v)+toAddValue > 75 {
+
+            ms.BoardPicture[player][k] = byte(75 - (int(v) + toAddValue - 75))
+
+            return int(ms.BoardPicture[player][k])
+        }
+        ms.BoardPicture[player][k] += byte(toAddValue)
+        return int(ms.BoardPicture[player][k])
+    }
+    // @TODO This is gonna be a bug one day
+    return 0
 }
 
 func (ms *MatchStatus) IsAWinner(player int) bool {
-	for _, pawnPosition := range ms.BoardPicture[player] {
-		if pawnPosition != BOXES {
-			return false
-		}
-	}
-	return true
+    for _, pawnPosition := range ms.BoardPicture[player] {
+        if pawnPosition != BOXES {
+            return false
+        }
+    }
+    return true
 }
 
 func (ms *MatchStatus) IsThereAPawnInJail(player int) bool {
-	for _, pawnPosition := range ms.BoardPicture[player] {
-		if pawnPosition == 0 {
-			return true
-		}
-	}
-	return false
+    for _, pawnPosition := range ms.BoardPicture[player] {
+        if pawnPosition == 0 {
+            return true
+        }
+    }
+    return false
 }
 
 func (ms *MatchStatus) GetPawnFromJail(player int) (int, error) {
-	for k, pawnPosition := range ms.BoardPicture[player] {
-		if pawnPosition == 0 {
-			return k, nil
-		}
-	}
-	return 0, errors.New("Not Pawn left in Jail")
+    for k, pawnPosition := range ms.BoardPicture[player] {
+        if pawnPosition == 0 {
+            return k, nil
+        }
+    }
+    return 0, errors.New("Not Pawn left in Jail")
 }
 
 func (ms *MatchStatus) GetMovablePawns(player int) []int {
-	var freePawns []int
-	for k, pawnPosition := range ms.BoardPicture[player] {
-		if pawnPosition != 0 && pawnPosition != BOXES {
-			freePawns = append(freePawns, k)
-		}
-	}
-	return freePawns
+    var freePawns []int
+    for k, pawnPosition := range ms.BoardPicture[player] {
+        if pawnPosition != 0 && pawnPosition != BOXES {
+            freePawns = append(freePawns, k)
+        }
+    }
+    return freePawns
 }
 
-func NewMatch() *Match {
-	id, _ := uuid.NewV4()
-	idString := id.String()
+type PlayerStatus map[int]byte
 
-	return &Match{
-		Id:          idString,
-		MatchStatus: NewMatchStatus(),
-	}
-}
 
-type TurnControl int
+
